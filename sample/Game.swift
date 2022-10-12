93e721f054d1af3a5c78414d48d831ef3d9096a1
Stephane Lefebvre <epiphane1772@gmail.com>
2022-10-11 17:23:22 -0700

    added morefighting

--- sample/Game.swift
+++ sample/Game.swift
@@ -60,13 +60,13 @@ final class Game {
     }
     
     func printtype() {
-       //  print(game.character)
+        //  print(game.character)
     }
     
     func recap() {
         func displayTeam(player: Player) {
             print("\(player.name)'s team:")
-            for i in 0...2 {
+            for i in 0...player.team.count - 1 {
                 print("\(player.team[i].name) weapon: \(player.team[i].weapon), life: \(player.team[i].life) ")
             }
         }
@@ -79,16 +79,18 @@ final class Game {
         
     }
     
-   func oneTeamIsDead() -> Bool {
-       if (isDead(character: player1!.team[0]) &&
-           isDead(character: player1!.team[1]) &&
-           isDead(character: player1!.team[2])) {
-           return true
-       }
-       if (isDead(character: player2!.team[0]) && isDead(character: player2!.team[1]) && isDead(character: player2!.team[2])) {
-           return true
-       }
-       return false
+    func oneTeamIsDead() -> Bool {
+        if (isDead(character: player1!.team[0]) &&
+            isDead(character: player1!.team[1]) &&
+            isDead(character: player1!.team[2])) {
+            return true
+        }
+        if (isDead(character: player2!.team[0]) &&
+            isDead(character: player2!.team[1]) &&
+            isDead(character: player2!.team[2])) {
+            return true
+        }
+        return false
     }
     
     func fight() {
@@ -107,6 +109,7 @@ final class Game {
                     choice = readLine()!
                     target = player1!.team[Int(choice)! - 1]
                     heal(character: target!)
+                    turn = 2
                     recap()
                 }
                 else {
@@ -115,9 +118,10 @@ final class Game {
                     choice = readLine()!
                     target = player2!.team[Int(choice)! - 1]
                     strike(character: target!)
+                    removeIfDead(character: target!)
                     recap()
+                    turn = 2
                 }
-                turn = 2
             }
             else {
                 print("Player 2:")
@@ -131,6 +135,7 @@ final class Game {
                     choice = readLine()!
                     target = player2!.team[Int(choice)! - 1]
                     heal(character: target!)
+                    turn = 1
                     recap()
                 }
                 else {
@@ -139,31 +144,33 @@ final class Game {
                     choice = readLine()!
                     target = player1!.team[Int(choice)! - 1]
                     strike(character: target!)
+                    removeIfDead(character: target!)
+                    turn = 1
                     recap()
                 }
-                turn = 2
+                
             }
         } while !oneTeamIsDead()
     }
     
     func displayTeam(player: Player) {
-        for i in 0...2 {
+        for i in 0...player.team.count - 1 {
             print("\(i + 1) for \(player.team[i]) )")
         }
     }
-        func isDead(character: Character) -> Bool {
-            var status = false
-            if character.life <= 0 {
-                status = true
-            }
-            else {
-                status = false
-            }
-            return status
+    func isDead(character: Character) -> Bool {
+        var status = false
+        if character.life <= 0 {
+            status = true
         }
-       
+        else {
+            status = false
+        }
+        return status
+    }
+    
     func chooseCharacter(player: Player) {
-        for i in 0...3 {
+        for i in 0...player.team.count - 1 {
             print("\(i + 1) for \(player.team[i])")
         }
     }
@@ -175,5 +182,17 @@ final class Game {
     func strike(character: Character) {
         character.life -= (striker?.weapon.strikeStrength)!
     }
-  }
-
+    
+    func removeIfDead(character: Character) {
+        for i in 1...(player2?.team.count)! - 1 {
+            if (player2?.team[i].life)! <= 0 {
+                player2?.team.remove(at: i)
+            }
+            for i in 0...(player1?.team.count)! - 1 {
+                if (player1?.team[i].life)! <= 0 {
+                    player1?.team.remove(at: i)
+                }
+            }
+        }
+    }
+}
